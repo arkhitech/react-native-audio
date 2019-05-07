73538d08d9700be481864f95e9328af3be38a097
Ahsan Jamil <ahsan.jamil@arkhitech.com>
2019-05-07 13:20:19 +0500

    commit to resolve not pulling issue

--- ios/AudioRecorderManager.m
+++ ios/AudioRecorderManager.m
@@ -130,10 +130,9 @@ return data;
   short audioFormat = 1;
 
   Byte *header = (Byte*)malloc(44);
-  
+
   FILE *fout;
 
-    
   if((fout = fopen([wavFilePath cStringUsingEncoding:1], "w")) == NULL)
   {
     printf("Error opening out file ");
